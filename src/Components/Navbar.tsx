eab9390ccbf88bff76377081acd773acab422af1
corbincargil <corbin.cargil@gmail.com>
2024-02-25 17:49:43 -0600

    add icons to navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,5 +1,16 @@
 import { NavLink } from "react-router-dom";
 import InterestsIcon from "@mui/icons-material/Interests";
+import { OverridableComponent } from "@mui/material/OverridableComponent";
+import { SvgIconTypeMap } from "@mui/material";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import AutoAwesomeMotionIcon from "@mui/icons-material/AutoAwesomeMotion";
+import BarChartIcon from "@mui/icons-material/BarChart";
+import ParkIcon from "@mui/icons-material/Park";
+import PersonIcon from "@mui/icons-material/Person";
+import SettingsIcon from "@mui/icons-material/Settings";
+import LoginIcon from "@mui/icons-material/Login";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
+import LogoutIcon from "@mui/icons-material/Logout";
 
 //todo: alternate navbar styles - delete once styling is decided
 // <nav className="top-0 left-0 min-h-screen h-full w-48 flex flex-col bg-slate-200 border border-r-gray-300 shadow-lg p-4">
@@ -8,33 +19,34 @@ import InterestsIcon from "@mui/icons-material/Interests";
 // const inactiveLinkClassNames = "text-blue-900 hover:underline rounded-md p-2";
 
 const activeLinkClassNames =
-  "bg-slate-200 border text-blue-900 border-slate-200 hover:underline rounded-lg p-2";
+  "flex gap-2 bg-slate-200 border text-blue-900 border-slate-200 hover:underline rounded-lg p-2";
 const inactiveLinkClassNames =
-  "hover:bg-slate-400  hover:ease-linear hover:duration-300 rounded-lg p-2";
+  "flex gap-2  my-6 text-slate-200 font-bold font-mono hover:bg-slate-400  hover:ease-linear hover:duration-300 rounded-lg p-2";
 // const inactiveLinkClassNames =
 //   " hover:border hover:border-slate-200 rounded-lg p-2";
 
 export interface NavigationLinkInterface {
   label: string;
   to: string;
+  icon?: OverridableComponent<SvgIconTypeMap<{}, "svg">>;
 }
 
 const primaryLinks: NavigationLinkInterface[] = [
-  { label: "Dashboard", to: "/dashboard" },
-  { label: "Posts", to: "/posts" },
-  { label: "Analytics", to: "/analytics" },
-  { label: "Evergreen", to: "/evergreen" },
+  { label: "Dashboard", to: "/dashboard", icon: DashboardIcon },
+  { label: "Posts", to: "/posts", icon: AutoAwesomeMotionIcon },
+  { label: "Analytics", to: "/analytics", icon: BarChartIcon },
+  { label: "Evergreen", to: "/evergreen", icon: ParkIcon },
 ];
 
 const secondaryLinks: NavigationLinkInterface[] = [
-  { label: "My profile", to: "/my-profile" },
-  { label: "Settings", to: "/settings" },
-  { label: "Log in", to: "/login" },
+  { label: "My profile", to: "/my-profile", icon: PersonIcon },
+  { label: "Settings", to: "/settings", icon: SettingsIcon },
+  { label: "Log in", to: "/login", icon: LoginIcon },
 ];
 
 const tertiaryLinks: NavigationLinkInterface[] = [
-  { label: "Help", to: "/help" },
-  { label: "Log out", to: "/logout" },
+  { label: "Help", to: "/help", icon: HelpOutlineIcon },
+  { label: "Log out", to: "/logout", icon: LogoutIcon },
 ];
 
 export default function Navbar() {
@@ -54,6 +66,7 @@ export default function Navbar() {
                   }
                   to={l.to}
                 >
+                  {l.icon && <l.icon fontSize="small" />}
                   {l.label}
                 </NavLink>
               </li>
@@ -70,6 +83,7 @@ export default function Navbar() {
                   }
                   to={l.to}
                 >
+                  {l.icon && <l.icon fontSize="small" />}
                   {l.label}
                 </NavLink>
               </li>
@@ -86,6 +100,7 @@ export default function Navbar() {
                   }
                   to={l.to}
                 >
+                  {l.icon && <l.icon fontSize="small" />}
                   {l.label}
                 </NavLink>
               </li>
